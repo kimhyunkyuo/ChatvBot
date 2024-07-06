7aebedbecb08989d10587a240372621f46a13402
giirrr <ikl1121@naver.com>
2024-07-06 17:13:23 +0900

    TEST: 커밋 메세지 테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsfsdf
+          Learn Reactsfsdfsffdfsf
         </a>
       </header>
     </div>
